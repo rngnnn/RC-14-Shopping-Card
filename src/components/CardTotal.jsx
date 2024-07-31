b5839ae17038aacff3927182da34a1f8663dd9df
rngnnn <rngnnnclk@gmail.com>
2024-07-31 21:19:28 +0200

    Card total.jsx

---
+++ src/components/CardTotal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const taxRate = 0.18;
+const shipping = 25;
+
+const CardTotal = ({urunler}) => {
+ 
+  const subTotal=urunler.reduce((toplam,item)=>item.price*item.dampingRate*item.amount+toplam,0)
+
+
+  return (
+    <table className="table w-100">
+      <tbody>
+        <tr className="text-end">
+          <th className="text-start">Subtotal</th>
+          <td>
+            $<span className="subtotal">{subTotal}</span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Tax(18%)</th>
+          <td>
+            $<span className="tax">{(subTotal*taxRate).toFixed(2)}</span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Shipping</th>
+          <td>
+            $<span className="shipping">{shipping} </span>
+          </td>
+        </tr>
+        <tr className="text-end">
+          <th className="text-start">Total</th>
+          <td>
+            $
+            <span className="total">
+              {(subTotal+subTotal*taxRate+shipping).toFixed(2)}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default CardTotal;
